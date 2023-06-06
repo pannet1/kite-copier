226bcb55d55252a2b9e71fc9d177b75249bcd943
karthick <prog@ecomsense.in>
2023-06-06 21:47:16 +0530

    add order log to order place

--- kite_copier/main.py
+++ kite_copier/main.py
@@ -107,10 +107,10 @@ def do_multiply(multiplied):
                 # quantity below freeze
                 elif iceberg > 0 and abs(quantity) < iceberg:
                     m['quantity'] = int(quantity)
-                    status = obj_usr.place_order(m)
+                    status = obj_usr.place_order(m, sec_dir)
             else:  # exchange is not NFO
                 m['quantity'] = int(quantity)
-                status = obj_usr.place_order(m)
+                status = obj_usr.place_order(m, sec_dir)
             logging.info(f'order: {status} {m}')
         except Exception as e:
             logging.warning(f"while multiplying {e}")
